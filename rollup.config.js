4d750d50dcfa86b2afe2f26817e5d3a927519e19
Miriam Hadfield <mhadfield@juniper.net>
2019-05-17 20:22:02 -0700

    Fixes gitignore

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import uglify from 'rollup-plugin-uglify'
+import babel from 'rollup-plugin-babel'
+
+const config = {
+    input: 'src/countdown.js',
+    external: ['react'],
+    plugins: [
+        babel({
+            exclude: "node_modules/**"
+        }),
+        uglify()
+    ],
+    output: {
+        format: 'umd',
+        name: 'countdown',
+        globals: {
+            react: "React"
+        }
+    }
+};
+export default config;
